a475d05741f8c6a7bf1476788892a0993dc09773
Matt Jenkins <mjenkins@tombras.com>
2025-04-25 15:57:24 -0400

    initial commit

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import { lazy, StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+
+const App = lazy(() => import("./App"));
+
+createRoot(document.getElementById("root")!).render(
+	<StrictMode>
+		<App />
+	</StrictMode>
+);
